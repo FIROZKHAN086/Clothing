f2c61b17075b4d2cfcce15afaceb94b19218d1dc
FIROZ KHAN <computerpc096@gmail.com>
2025-03-03 15:50:40 -0800

    devl..

--- frontant/vite.config.js
+++ frontant/vite.config.js
@@ -1,25 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      external: [],
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  optimizeDeps: {
-    include: ['@headlessui/react']
-  }
-})
+})  
+
+
